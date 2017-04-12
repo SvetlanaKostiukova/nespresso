ceac97c94d77daf268e82fbb0789ae5b1ca7c831
Svetlana <svetlanak@mstlab.org>
2017-04-13 02:18:12 +0300

    update

--- src/app/timeline/timeline.component.ts
+++ src/app/timeline/timeline.component.ts
@@ -6,6 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./timeline.component.less']
 })
 export class TimelineComponent implements OnInit {
+  selectedYear:string = "800 г.";
   years:string[] = ["800 г.", "1545–1550 гг.", "1800 г.", "1827 г.", "1906–1985 гг.", "1986–2017 гг."];
 
   constructor() { }
